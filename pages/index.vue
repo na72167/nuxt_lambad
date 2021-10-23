c09ae2b6f643008ba854510ec3e6c355ebc9830f
yh72167 <yh.19972372@gmail.com>
2021-10-23 21:16:43 +0900

    最初のコミット

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div> {{ message }} </div>
+        <button @click="click">ボタン</button>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import axios from 'axios'
+export default Vue.extend({
+    data(): {
+      message: unknown
+    } {
+      return {
+        message: ''
+      }
+    },
+    methods: {
+        async click():Promise<void> {
+        const response = await axios.get('https://cldu07velj.execute-api.ap-northeast-1.amazonaws.com/prod/item')
+        this.message = response.data
+        }
+    }
+})
+</script>
